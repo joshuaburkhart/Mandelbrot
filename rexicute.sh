eb4e78a0d023d9c7b8436e0e83c7da5d50520f0f
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-01-29 21:49:45 -0800

    removed old script

--- rexicute.sh
+++
@@ -1,9 +0,0 @@
-mpirun -np $1 ./mandelbrot -x $2 -y $3 -s $4 -w $5 -h $6
-R --no-save << EOT
-A = scan(file="output.csv",sep=",")
-A = matrix(A,nrows=$6,ncol=$5)
-A = A[,ncol(A):1]
-image(A)
-quit("no")
-EOT
-evince "./Rplots.pdf"
